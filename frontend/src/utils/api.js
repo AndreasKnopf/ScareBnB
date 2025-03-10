15710e6c2fdb47bf85343df14d909c786a6a3873
Andreas Knopf <knopfandreas@arcor.de>
2025-03-10 16:29:33 +0100

    repair env files frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const deployed =
+  import.meta.env.VITE_API_DEPLOYED === 'true'
+    ? import.meta.env.VITE_API_RENDER_BACKEND_URL
+    : import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: deployed,
   withCredentials: true,
 });
 
